c0e5e03c5dc673a3214da21befc59ac525a6e2f5
Philip Robinson <probinso@localhost.localdomain>
2014-02-19 21:27:53 -0800

    rules first draft, not done

--- main.py
+++ main.py
@@ -12,3 +12,27 @@ def altchars(data):
     return retval
 
 indata = altchars(indata)
+
+def preservewhite(data):
+    for index, ch in enumerate(data):
+        if not str.isspace(ch): break
+    return data[:index], data[index:]
+
+def getword(data):
+    for index, ch in enumerate(data):
+        if str.isspace(ch): break
+    return data[:index], data[index:]
+
+def rules(data):
+    acc, rest = preservewhite(data)
+    word, rest = getword(rest)
+
+    k = 0
+    for index, ch in enumerate(word):
+        if isVowel(ch):
+            acc += str.lower(ch)
+            k = index
+        elif str.lower(ch) is 'l':
+            acc += 'L'
+        else:
+            pass
