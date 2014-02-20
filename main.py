fa009f59196d5236d9b7feee0a16c900289d5b2f
Philip Robinson <probinso@localhost.localdomain>
2014-02-19 17:39:31 -0800

    altchars function

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 
 
 fd = open("bibleverse.txt")
+indata = fd.read()
+fd.close()
+
+def altchars(data):
+    f = lambda index, char : (str.upper, str.lower)[index%2](char)
+    retval = ""
+    for index, char in enumerate(data):
+        retval += f(index, char)
+    return retval
+
+indata = altchars(indata)
